a3c02fa1c49200a3150ed31f0fb43c8f4bb27713
Sarwar Ahmed <sarwar.amas@gmail.com>
2024-05-02 20:02:17 +0600

    Setup Firebase Hosting

--- src/components/MyList.jsx
+++ src/components/MyList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 
 const MyList = () => {
@@ -7,6 +7,12 @@ const MyList = () => {
     console.log('myList', myList.id);
 
     console.log(myList);
+
+    useEffect(() => {
+        document.title = 'Himaloy - My List';
+    }, []);
+
+
     return (
         <div>
             {/* my list */}
@@ -16,12 +22,18 @@ const MyList = () => {
                         <h2 className="card-title">My List</h2>
                         <ul className='grid md:grid-cols-3 gap-5'>
                             {myList.map((spot, index) => (
-                                < Link to={`/spot/${spot._id}`} key={index}>
+                                <div key={index}>
                                     <li key={index}>
                                         <img className='' src={spot.image} alt={spot.touristSpotName} />
                                         <p>{spot.touristSpotName}</p>
                                     </li>
-                                </Link>
+                                    <div className="flex justify-start space-x-4 mt-4">
+                                        <Link to={`/viewSpot/${spot._id}`} className="btn btn-primary">View</Link>
+                                        <Link to={`/editSpot/${spot._id}`} className="btn btn-success">Update</Link>
+                                        {/* <Link to={`/deleteSpot/${spot._id}`} className="btn btn-error">Delete</Link> */}
+                                        <button className="btn btn-error">Delete</button>
+                                    </div>
+                                </div>
                             ))}
                         </ul>
                     </div>
